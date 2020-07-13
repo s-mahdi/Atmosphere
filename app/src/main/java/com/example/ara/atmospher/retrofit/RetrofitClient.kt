1fe94e0818708c30dbbf47325b8d184e910f97ac
s-mahdi <smmh1376@gmail.com>
2020-07-13 22:34:13 +0430

    :new: retrofit client created

---
+++ app/src/main/java/com/example/ara/atmospher/retrofit/RetrofitClient.kt
@@ -0,0 +1,14 @@
+package com.example.ara.atmospher.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    val instance: OpenWeatherService by lazy {
+         val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.openweathermap.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return@lazy retrofit.create(OpenWeatherService::class.java)
+    }
+}
