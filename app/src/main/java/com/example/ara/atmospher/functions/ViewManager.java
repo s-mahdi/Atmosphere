47a5bb87ae3e6e144e433ba04f80acd9aa5fbfee
s-mahdi <smmh1376@gmail.com>
2020-07-13 22:36:24 +0430

    :new: functions added

--- app/src/main/java/com/example/ara/atmospher/functions/ViewManager.java
+++ app/src/main/java/com/example/ara/atmospher/functions/ViewManager.java
@@ -1,5 +1,7 @@
 package com.example.ara.atmospher.functions;
 
+import android.app.Activity;
+import android.content.Context;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
@@ -12,11 +14,24 @@ public class ViewManager {
             view.setVisibility(View.INVISIBLE);
     }
 
+    public void hideView(View view) {
+        view.setVisibility(View.INVISIBLE);
+    }
+
+    public void showView(View view) {
+        view.setVisibility(View.VISIBLE);
+    }
+
     public void toggleSoftKeyboard(InputMethodManager imm) {
         if (imm.isActive())
             imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
         else
             imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0);
 
+    }
+
+    public void hideKeyboardFrom(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
