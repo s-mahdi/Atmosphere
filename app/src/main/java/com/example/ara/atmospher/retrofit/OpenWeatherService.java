ccae2c10caac9342ee307362eb5b48ad7059f260
s-mahdi <smmh1376@gmail.com>
2020-07-13 22:34:01 +0430

    :arrow_forward: file location changed

--- app/src/main/java/com/example/ara/atmospher/retrofit/OpenWeatherService.java
+++ app/src/main/java/com/example/ara/atmospher/retrofit/OpenWeatherService.java
@@ -1,4 +1,4 @@
-package com.example.ara.atmospher.Interfaces;
+package com.example.ara.atmospher.retrofit;
 
 import com.example.ara.atmospher.WeatherData;
 
@@ -10,5 +10,8 @@ public interface OpenWeatherService {
 
     @GET("/data/2.5/weather")
     Call<WeatherData> weatherCall(@Query("q") String cityName, @Query("appid") String API_KEY);
+
+    @GET("data/2.5/forecast/daily")
+    Call<WeatherData> forecastCall(@Query("q") String cityName, @Query("lang") String lang, @Query("appid") String API_KEY);
 
 }
