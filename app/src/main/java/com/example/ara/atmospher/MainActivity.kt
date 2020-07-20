a3530bfb5dfd143066f16bfc45b82ff475707c57
s-mahdi <smmh1376@gmail.com>
2020-07-20 21:46:23 +0430

    :tada: #4 Drawer added. encapsulation

--- app/src/main/java/com/example/ara/atmospher/MainActivity.kt
+++ app/src/main/java/com/example/ara/atmospher/MainActivity.kt
@@ -1,38 +1,28 @@
 package com.example.ara.atmospher
 
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.View
 import android.view.View.OnFocusChangeListener
-import android.view.Window
-import android.view.WindowManager
-import android.view.inputmethod.InputMethodManager
-import android.widget.*
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.ara.atmospher.events.ClickManager
-import com.example.ara.atmospher.functions.*
-import com.example.ara.atmospher.model.WeatherData
+import com.example.ara.atmospher.events.KeyManager
+import com.example.ara.atmospher.functions.ViewManager
+import com.example.ara.atmospher.functions.hideStatusBar
+import com.example.ara.atmospher.functions.onSearch
+import com.example.ara.atmospher.functions.syncStorage
 import com.example.ara.atmospher.viewModels.MainViewModel
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-    private var weatherData: WeatherData? = null
-    var navView: NavigationView? = null
-
+    //    var navView: NavigationView? = null
     private var CITY_NAME: String? = null
-    private var cityNameTextView: TextView? = null
-    private var tempTextView: TextView? = null
-    private var maxTempTextView: TextView? = null
-    private var minTempTextView: TextView? = null
-    private var climateConditionTextView: TextView? = null
     private var climateConditionImageView: ImageView? = null
     private var backGroundImageView: ImageView? = null
     private var addCityImageButton: ImageButton? = null
@@ -43,12 +33,10 @@ class MainActivity : AppCompatActivity() {
     private var searchInput // used for city name
             : EditText? = null
     private var mDrawerLayout: DrawerLayout? = null
-    private var icon: Icon? = null
-    private var condition: Condition? = null
-    private var imm: InputMethodManager? = null
     private var viewManager: ViewManager? = null
     private var sharedPref: SharedPreferences? = null
     private var clickManager: ClickManager? = null
+    private var keyManager: KeyManager? = null
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,21 +44,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         //hide status bar
-        hideStatusBar(this);
+//        hideStatusBar(this);
         setContentView(R.layout.activity_main)
 
         initializer()
+
+        viewManager?.setSlider(this);
 
         setEvents()
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        viewModel.weatherDate.observe(this, Observer {setView(it)})
+        viewModel.weatherDate.observe(this, Observer {
+            viewManager?.setWeatherView(it, this)
+        })
 
         viewModel.setCityName("yazd")
 
         CITY_NAME = syncStorage(sharedPref!!)
-        configureNavigationDrawer()
     }
 
     override fun onDestroy() {
@@ -78,56 +69,26 @@ class MainActivity : AppCompatActivity() {
         viewModel.cancelJobs()
     }
 
-    private fun configureNavigationDrawer() {
-        navView!!.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener { menuItem ->
-            val f: Fragment? = null
-            val itemId = menuItem.itemId
-            if (itemId == R.id.temperatureUnit) {
-                // TODO: 19/01/2019 add some fragments to this
-                Toast.makeText(this@MainActivity, "Clicked", Toast.LENGTH_SHORT).show()
-            } else if (itemId == R.id.locations) {
-                Toast.makeText(this@MainActivity, "Clicked", Toast.LENGTH_SHORT).show()
-            }
-            if (f != null) {
-                mDrawerLayout?.closeDrawers()
-                return@OnNavigationItemSelectedListener true
-            }
-            false
-        })
-    }
-
     private fun setEvents() {
-        addCityImageButton!!.setOnClickListener(clickManager)
-        searchButton!!.setOnClickListener(clickManager)
-        closeSearchPlotImageButton!!.setOnClickListener(clickManager)
-        drawerHamburgerImageButton!!.setOnClickListener(clickManager)
-        backGroundImageView!!.setOnClickListener(clickManager)
-        searchInput!!.setOnKeyListener(View.OnKeyListener { _, _, event ->
-            if (event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_ENTER) {
-                viewManager!!.toggleView(searchBar!!)
-                viewManager!!.toggleSoftKeyboard(imm!!)
-//                handleRequest(searchInput!!.text.toString())
-                searchInput!!.setText("")
-
+        addCityImageButton?.setOnClickListener(clickManager)
+        searchButton?.setOnClickListener(clickManager)
+        closeSearchPlotImageButton?.setOnClickListener(clickManager)
+        drawerHamburgerImageButton?.setOnClickListener(clickManager)
+        backGroundImageView?.setOnClickListener(clickManager)
+        searchInput?.setOnKeyListener(KeyManager(this) {
+            onSearch(this) {
                 viewModel.setCityName(searchInput?.text.toString())
-                return@OnKeyListener true
             }
-            false
         })
-        searchInput!!.onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
+        searchInput?.onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
-                searchBar!!.visibility = View.INVISIBLE
+                searchBar?.visibility = View.INVISIBLE
             }
         }
     }
 
     private fun initializer() {
         // set views
-        cityNameTextView = findViewById(R.id.textView_cityName)
-        tempTextView = findViewById(R.id.textView_temperature)
-        maxTempTextView = findViewById(R.id.textView_maxTemp)
-        minTempTextView = findViewById(R.id.textView_minTemp)
-        climateConditionTextView = findViewById(R.id.textView_climateCondition)
         climateConditionImageView = findViewById(R.id.imageView_climateCondition)
         addCityImageButton = findViewById(R.id.imageButton_addCity)
         searchBar = findViewById(R.id.searchPlot)
@@ -137,30 +98,20 @@ class MainActivity : AppCompatActivity() {
         drawerHamburgerImageButton = findViewById(R.id.imageButton_drawer_hamburger)
         backGroundImageView = findViewById(R.id.imageView_background)
         mDrawerLayout = findViewById(R.id.drawer_layout)
-        navView = findViewById(R.id.navigation)
-        icon = Icon()
-        condition = Condition()
         viewManager = ViewManager()
         sharedPref = getSharedPreferences(
                 getString(R.string.city_key), Context.MODE_PRIVATE)
+
         clickManager = ClickManager(this) {
             onSearch(this) {
                 viewModel.setCityName(searchInput?.text.toString())
             }
         }
-        imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        keyManager = KeyManager(this) {
+            onSearch(this) {
+                viewModel.setCityName(searchInput?.text.toString())
+            }
+        }
     }
 
-    @SuppressLint("SetTextI18n")
-    fun setView(weatherData: WeatherData) {
-        val id = weatherData.weatherList[0].id
-        mDrawerLayout!!.visibility = View.VISIBLE
-        cityNameTextView!!.text = weatherData.cityName
-        tempTextView!!.text = (weatherData.mainCondition.temperature.toInt() - 273).toString() + "°"
-        maxTempTextView!!.text = (weatherData.mainCondition.temp_max.toInt() - 273).toString()
-        minTempTextView!!.text = (weatherData.mainCondition.temp_min.toInt() - 273).toString()
-        climateConditionTextView!!.text = condition!!.getCondition(id)
-        climateConditionImageView!!.setImageDrawable(getDrawable(icon!!.getIcon(id)))
-        backGroundImageView!!.setImageDrawable(getDrawable(getBackground(id)))
-    }
 }
