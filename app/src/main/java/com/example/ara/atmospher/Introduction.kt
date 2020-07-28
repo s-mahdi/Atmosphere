d73699a300339265cc56603be3de2852df5a130e
s-mahdi <smmh1376@gmail.com>
2020-07-28 22:17:13 +0430

    ✈️ #2 migration completed. closing #2

---
+++ app/src/main/java/com/example/ara/atmospher/Introduction.kt
@@ -0,0 +1,29 @@
+package com.example.ara.atmospher
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class Introduction : AppCompatActivity() {
+    var letsGoButton: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val i = Intent(this@Introduction, MainActivity::class.java)
+        val sharedPreferences = getSharedPreferences("com.example.ara.atmospher", Context.MODE_PRIVATE)
+        if (sharedPreferences.getString("isLogedIn", false.toString()) != "true") {
+            setContentView(R.layout.activity_introduction)
+            sharedPreferences.edit().putString("isLogedIn", "true").apply()
+            letsGoButton = findViewById(R.id.button_start)
+            letsGoButton.setOnClickListener(View.OnClickListener {
+                startActivity(i)
+                finish()
+            })
+        } else {
+            startActivity(i)
+            finish()
+        }
+    }
+}
