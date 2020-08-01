7a8cf6a60595ca3fca024a1f1d350d7ea8b879d1
s-mahdi <smmh1376@gmail.com>
2020-08-01 12:25:16 +0430

    🌐 view model updated

--- app/src/main/java/com/example/ara/atmospher/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/ara/atmospher/viewModels/MainViewModel.kt
@@ -1,12 +1,9 @@
 package com.example.ara.atmospher.viewModels
 
-import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import com.example.ara.atmospher.models.openWeather.Current
-import com.example.ara.atmospher.models.openWeather.Forecast5
 import com.example.ara.atmospher.models.openWeather.oneCall.OneCall
 import com.example.ara.atmospher.models.opencage.Geometry
 import com.example.ara.atmospher.models.opencage.OpenCageResult
@@ -17,16 +14,6 @@ class MainViewModel : ViewModel() {
     private val _geometry: MutableLiveData<Geometry> = MutableLiveData()
 
     // whenever `_cityName` changed run the `getWeather` from Repository
-    val weatherDate: LiveData<Current?> = Transformations
-            .switchMap(_cityName) { cityName ->
-                Repository.getWeather(cityName)
-            }
-
-    val forecastData: LiveData<Forecast5?> = Transformations
-            .switchMap(_cityName) { cityName ->
-                Repository.getForecast(cityName)
-            }
-
     val oneCallData: LiveData<OneCall?> = Transformations
             .switchMap(_geometry) { geometry ->
                 Repository.oneCall(geometry.lat, geometry.lng)
