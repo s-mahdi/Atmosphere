e5307caf543fcf1e2e34d04fd9e3cd8e8ce9cec5
s-mahdi <smmh1376@gmail.com>
2020-08-01 12:14:25 +0430

    #9 💫 acitivy added

---
+++ app/src/main/java/com/example/ara/atmospher/activities/SettingsActivity.kt
@@ -0,0 +1,26 @@
+package com.example.ara.atmospher.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import com.example.ara.atmospher.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+    }
+
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+
+    }
+}
